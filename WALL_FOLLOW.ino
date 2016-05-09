89eb97eee0182fa64e54740f91f4db9a3ee02c24
lallo188 <lallo188@gmail.com>
2016-05-09 06:52:46 -0400

    Create WALL_FOLLOW.ino

---
+++ WALL_FOLLOW.ino
@@ -0,0 +1,10 @@
+void followWall(int identity){
+  if (checkUS(identity) =< 10){
+    drive(30,30);  
+  } else{
+    if (identity == 0){
+      drive(0,35);
+    } else if (identity == 2){
+      drive(35,0);
+    }
+}
